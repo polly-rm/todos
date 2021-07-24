a4b635370a061bac3cd4ddcc4a97adb589411f02
Polina Mincheva <polina_rm@abv.bg>
2021-07-24 16:25:27 +0300

    adding files

---
+++ todos_app/todos_app/todos/forms.py
@@ -0,0 +1,82 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MinLengthValidator
+
+from todos_app.todos.models import Todo
+from todos_app.todos.validators import validate_owner_todos_count
+
+
+def validate_bot_catcher_empty(value):
+    if value:
+        raise ValidationError('You are a bot')
+
+
+#
+# def validate_min_length(min_length):
+#     def validate(value):
+#         if len(value) < min_length:
+#             raise forms.ValidationError
+#
+#     return validate
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+        # fields = ('title', 'state', 'description', 'owner')
+        # exclude = ('categories',)
+        widgets = {
+            'title': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                },
+            ),
+            'owner': forms.RadioSelect(),
+        }
+
+    def clean(self):
+        validate_owner_todos_count(self.cleaned_data['owner'])
+
+    # def clean_title(self):
+    #     validate_dot(self.cleaned_data['title'])
+
+
+class CreateTodoForm(forms.Form):
+    title = forms.CharField(
+        max_length=30,
+        validators=[
+            # validate_dot,validate_dot
+            MinLengthValidator(3),
+            # validate_min_length(5),
+        ],
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter todo text',
+            }
+        )
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'my-text-area',
+                'rows': 3,
+            }
+        )
+    )
+
+    bots_catcher = forms.CharField(
+        widget=forms.HiddenInput(),
+        required=False,
+        validators=[
+            # validate_bot_catcher_empty,
+        ]
+    )
+
+    def clean_bots_catcher(self):
+        value = self.cleaned_data['bots_catcher']
+        validate_bot_catcher_empty(value)
+
+
+class UpdateTodoForm(CreateTodoForm):
+    state = forms.BooleanField()
---
+++ todos_app/todos_app/todos/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render, redirect
+
+from todos_app.todos.forms import CreateTodoForm, UpdateTodoForm, TodoForm
+
+#
+# def index(request):
+#     context = {
+#         'todos': Todo.objects.all(),
+#         'form': CreateTodoForm(),
+#     }
+#
+#     return render(request, 'index.html', context)
+#
+#
+# def create_todo(request):
+#     form = CreateTodoForm(request.POST)
+#
+#     if form.is_valid():
+#         # cleaned_data is available ONLY
+#         # after the `i0s_valid()` call
+#         text = form.cleaned_data['text']
+#         description = form.cleaned_data['description']
+#         todo = Todo(
+#             text=text,
+#             description=description,
+#             # owner=owner,
+#         )
+#         todo.save()
+#         return redirect('/')
+#     else:
+#         context = {
+#             'todos': Todo.objects.all(),
+#             'form': form,
+#         }
+#
+#         return render(request, 'index.html', context)
+#
+#
+# def change_todo_state(request, pk):
+#     todo = Todo.objects.get(pk=pk)
+#     todo.state = not todo.state
+#     todo.save()
+#     return redirect('/')
+from todos_app.todos.models import Todo
+
+
+def index(request):
+    context = {
+        'todos': Todo.objects.all(),
+    }
+    return render(request, 'todo_app/index.html', context)
+
+
+def create_todo(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = TodoForm()
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'todo_app/create.html', context)
+
+
+def show_form(request, form):
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'todo_app/edit.html', context)
+
+
+def update_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+
+    if request.method == 'GET':
+        return show_form(request, TodoForm(initial=todo.__dict__))
+    else:
+        form = TodoForm(request.POST, instance=todo)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+        return show_form(request, form)
